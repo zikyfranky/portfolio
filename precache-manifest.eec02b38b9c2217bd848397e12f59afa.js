7786d5bd020a97e5cbdaee1df812b55e0b8322fb
Isaac Frank <masturisaacfrank@gmail.com>
2020-12-23 14:23:35 +0100

    portfolio

---
+++ precache-manifest.eec02b38b9c2217bd848397e12f59afa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb40a6668d1fb821be480948a1c11ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13ff2bcd777da99a0de4",
+    "url": "/static/js/2.a3249967.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.a3249967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4013e12ba9ede89c4143",
+    "url": "/static/js/main.78219e4e.chunk.js"
+  },
+  {
+    "revision": "cdb94f8e4b7c8fb4fd9d",
+    "url": "/static/js/runtime-main.519dc993.js"
+  }
+]);
